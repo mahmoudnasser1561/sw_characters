565a602c589c91e54795391b3dcd737d27cc0734
mahmoudnasser1561@gmail.com <mahmoudnasser1561@gmail.com>
2024-12-05 18:05:33 +0200

    Built the parse_char

--- interface.py
+++ interface.py
@@ -9,7 +9,12 @@ def search(name='luke'):
     print(f'Cannot find the character "{name}"')
 
 def parse_char(char):
-  pass
+  char_name = search_api.parse_name(char)
+  planet= search_api.parse_planet(char)
+  film_list = search_api.parse_films(char)
+  titles = search_api.format_titles(film_list)
+  description = search_api.person_description(char_name, planet, titles)
+  return description
 
 if __name__ == '__main__':
   fire.Fire(search)
